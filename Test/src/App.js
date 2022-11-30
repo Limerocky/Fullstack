60757eb3d7c8d808958297699a1a1685b0062888
Limerocky <113631571+Limerocky@users.noreply.github.com>
2022-11-30 14:08:00 +0200

    2.11

--- Test/src/App.js
+++ Test/src/App.js
@@ -1,11 +1,23 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-import { useState } from 'react'
-import { eventWrapper } from '@testing-library/user-event/dist/utils'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+
+  const hook = () => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }
+
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
 
 
   const addNote = (event) => {
@@ -21,10 +33,12 @@ const App = (props) => {
     setNewNote('')
   }
 
+
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
