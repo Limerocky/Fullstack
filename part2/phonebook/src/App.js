86732f5d7b00d011b628d61884b637d69fac6649
Limerocky <113631571+Limerocky@users.noreply.github.com>
2022-12-06 17:03:57 +0200

    2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -40,9 +40,30 @@ const List = ({ persons, filter, setPersons }) => {
   )
 }
 
+const Message = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  let cName = ''
+
+  if (message[0] === 'A') {
+    cName = 'success'
+  }
+  else if (message[0] === 'E') {
+    cName = 'error'
+  }
+
+  return (
+    <div className={cName}>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
 
 
   const addNewPerson = (event) => {
@@ -54,14 +75,23 @@ const App = () => {
 
     if (persons.some(e => e.name === person.name)) {
       const id = persons.filter(existing => existing.name === person.name)[0].id
-      personService.update(id, person).then(
-        setPersons(persons.filter(e => e.id !== id).concat(person))
-      )
+      personService.update(id, person).catch(error => {
+        setMessage(`ERROR ${person.name} has been removed from the server. Refresh the page to add them back.`)
+
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+      })
+      setPersons(persons.filter(e => e.id !== id).concat(person))
     }
     else {
       personService.create(person).then(returnedPerson => {
         setPersons(persons.concat(returnedPerson))
       })
+      setMessage(`Added ${person.name}`)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
     } 
   }
 
@@ -80,11 +110,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Message message={message} />
       <div>
         filter shown with <input value={filter} onChange={onFilterChange}/>
       </div>
       <h2>add a new</h2>
-      <Form addNewPerson={addNewPerson}/>
+      <Form addNewPerson={addNewPerson} />
       <h2>Numbers</h2>
       <List persons={persons} filter={filter} setPersons={setPersons}/>
     </div>
